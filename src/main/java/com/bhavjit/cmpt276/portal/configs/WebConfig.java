2517abbe0ce63c780c2a8aa24d13022c9fedd47f
Alex Noh <90161063+ndh707@users.noreply.github.com>
2024-07-22 15:15:16 -0700

    HOTFIX: add cmpt276.bhavjit.com to list of allowed domains in CORS config

--- src/main/java/com/bhavjit/cmpt276/portal/configs/WebConfig.java
+++ src/main/java/com/bhavjit/cmpt276/portal/configs/WebConfig.java
@@ -14,8 +14,10 @@ public class WebConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
         // TODO: change allowed origins
-            .allowedOrigins("http://localhost:5173", "http://aws-yyc-app-client.s3-website-us-west-2.amazonaws.com",
-                            "https://dd06gmwf4nvya.cloudfront.net")
+            .allowedOrigins("http://localhost:5173", 
+                            "http://aws-yyc-app-client.s3-website-us-west-2.amazonaws.com",
+                            "https://dd06gmwf4nvya.cloudfront.net", 
+                            "cmpt276.bhavjit.com")
             .allowedMethods("*")
             .allowedHeaders("*")
             .allowCredentials(true);
